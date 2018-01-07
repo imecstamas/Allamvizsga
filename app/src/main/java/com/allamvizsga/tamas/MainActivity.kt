06765f992ec030cc978684dad783a340fc1564ef
Tamas Imecs <tamas@halcyonmobile.com>
2018-01-07 23:34:27 +0100

    Add Crashlytics

--- app/src/main/java/com/allamvizsga/tamas/MainActivity.kt
+++ app/src/main/java/com/allamvizsga/tamas/MainActivity.kt
@@ -2,11 +2,14 @@ package com.allamvizsga.tamas
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener { Crashlytics.getInstance().crash() }
     }
 }
