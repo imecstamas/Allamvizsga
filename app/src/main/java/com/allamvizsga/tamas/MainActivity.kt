b3a884565e2d886b63bd05721785967a368af0e7
Tamas Imecs <tamas@halcyonmobile.com>
2018-01-21 23:30:45 +0100

    Enable dataBinding

--- app/src/main/java/com/allamvizsga/tamas/MainActivity.kt
+++ app/src/main/java/com/allamvizsga/tamas/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.allamvizsga.tamas
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.allamvizsga.tamas.databinding.ActivityMainBinding
 import com.allamvizsga.tamas.model.Coordinate
 import com.allamvizsga.tamas.model.Station
 import com.allamvizsga.tamas.model.Walk
 import com.allamvizsga.tamas.storage.repository.WalkRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 
@@ -18,41 +19,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+            uploadButton.setOnClickListener {
+                walkRepository.saveWalk(Walk(title = "Kolozsvari seta 3",
+                        description = "Kicsi seta kicsi seta 3",
+                        imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699",
+                        stations = arrayListOf(
+                                Station(title = "Allomas", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699"),
+                                Station(title = "Allomas2", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699"),
+                                Station(title = "Allomas 3", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699")
+                        )))
+            }
 
-        upload_button.setOnClickListener {
-            walkRepository.saveWalk(Walk(title = "Kolozsvari seta 3",
-                    description = "Kicsi seta kicsi seta 3",
-                    imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699",
-                    stations = arrayListOf(
-                            Station(title = "Allomas", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699"),
-                            Station(title = "Allomas2", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699"),
-                            Station(title = "Allomas 3", coordinate = Coordinate(42.56, -38.98), description = "Allomas leiras", imageUrl = "https://firebasestorage.googleapis.com/v0/b/allamvizsga-4e26b.appspot.com/o/pictures%2Fregi.jpg?alt=media&token=2b585a16-42d8-4218-9801-0b121232a699")
-                    )))
-        }
+            var id = ""
+            downloadAllButton.setOnClickListener {
+                walkRepository.getAll()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ walks ->
+                            println("Walks: $walks")
+                            id = walks[0].id!!
+                        }, { error ->
+                            error.printStackTrace()
+                        })
+            }
 
-        var id = ""
-        download_all_button.setOnClickListener {
-            walkRepository.getAll()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({ walks ->
-                        println("Walks: $walks")
-                        id = walks[0].id!!
-                    }, { error ->
-                        error.printStackTrace()
-                    })
-        }
-
-        download_one_button.setOnClickListener {
-            walkRepository.getById(id)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({ walk ->
-                        println("Walk: $walk")
-                    }, { error ->
-                        error.printStackTrace()
-                    })
+            downloadOneButton.setOnClickListener {
+                walkRepository.getById(id)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ walk ->
+                            println("Walk: $walk")
+                        }, { error ->
+                            error.printStackTrace()
+                        })
+            }
         }
     }
 }
